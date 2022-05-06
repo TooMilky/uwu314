27e6425cc73ef3823bfa0ec445940eb5e14ebd06
Thinking Milk <32953777+TooMilky@users.noreply.github.com>
2022-05-06 15:36:54 +0800

    add food details

--- app/src/main/java/com/uwu/csit314/Common/Common.java
+++ app/src/main/java/com/uwu/csit314/Common/Common.java
@@ -6,4 +6,5 @@ public class Common {
     public static User currentUser;
     public static final String USER_KEY = "User";
     public static final String PWD_KEY = "Password";
+    public static final String INTENT_FOOD_ID = "FoodId";
 }
--- app/src/main/java/com/uwu/csit314/FoodDetail.java
+++ app/src/main/java/com/uwu/csit314/FoodDetail.java
@@ -1,4 +1,247 @@
 package com.uwu.csit314;
 
-public class FoodDetail {
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.andremion.counterfab.CounterFab;
+//import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+import com.uwu.csit314.Common.Common;
+import com.uwu.csit314.Model.Food;
+import com.uwu.csit314.Model.Order;
+
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Arrays;
+
+public class FoodDetail extends AppCompatActivity {
+
+    TextView food_name, food_price, food_description;
+    ImageView food_image;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+//    FloatingActionButton btn_rating;
+//    ElegantNumberButton numberButton;
+
+//    RatingBar ratingBar;
+
+    String FoodId = "";
+    FirebaseDatabase database;
+    DatabaseReference foods;
+    DatabaseReference ratingTbl;
+    Food currentFood;
+    CounterFab btnCart;
+
+//    FButton btnShowComment;
+
+//    @Override
+//    protected void attachBaseContext(Context newBase) {
+//        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+//                .setDefaultFontPath("fonts/restaurant_font.otf")
+//                .setFontAttrId(R.attr.fontPath)
+//                .build());
+//        setContentView(R.layout.activity_cart);
+
+        setContentView(R.layout.activity_food_detail);
+//        btnShowComment = (FButton)findViewById(R.id.btnShowComment);
+//        btnShowComment.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(FoodDetail.this, ShowComment.class);
+//                intent.putExtra(Common.INTENT_FOOD_ID, FoodId);
+//                startActivity(intent);
+//            }
+//        });
+
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        foods = database.getReference("Foods");
+        ratingTbl = database.getReference("Rating");
+
+//        numberButton = (ElegantNumberButton)findViewById(R.id.number_button);
+        btnCart = (CounterFab) findViewById(R.id.btnCart);
+//        btn_rating = (FloatingActionButton)findViewById(R.id.btn_rating);
+//        ratingBar = (RatingBar)findViewById(R.id.RatingBar);
+
+//        btn_rating.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                showRatingDialog();
+//            }
+//        });
+
+//        btnCart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                new Database(getBaseContext()).addToCart(new Order(
+//                        FoodId,
+//                        currentFood.getName(),
+//                        numberButton.getNumber(),
+//                        currentFood.getPrice(),
+//                        currentFood.getDiscount(),
+//                        currentFood.getImage()
+//                ));
+//                Toast.makeText(FoodDetail.this, "Added to Cart", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+//        btnCart.setCount(new Database(this).getCountCart());
+        food_description = (TextView)findViewById(R.id.food_description);
+        food_name = (TextView)findViewById(R.id.food_name);
+        food_price = (TextView)findViewById(R.id.food_price);
+        food_image = (ImageView)findViewById(R.id.img_food);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppBar);
+
+        //Get Food Id
+        if (getIntent() != null)
+            FoodId = getIntent().getStringExtra("FoodId");
+        if (!FoodId.isEmpty() && FoodId != null) {
+//            if(Common.isConnectedToInternet(this)) {
+                getDetailFood(FoodId);
+//                getRatingFood(FoodId);
+//            }
+//            else
+//            {
+//                Toast.makeText(FoodDetail.this, "Please check your connection", Toast.LENGTH_SHORT).show();
+//                return;
+//            }
+        }
+
+
+    }
+
+//    private void getRatingFood(String foodId) {
+//        Query foodRating = ratingTbl.orderByChild("foodId").equalTo(foodId);
+//        foodRating.addValueEventListener(new ValueEventListener() {
+//            int count = 0, sum = 0;
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+//                    Rating item = postSnapshot.getValue(Rating.class);
+//                    sum += Integer.parseInt(item.getRateValue());
+//                    count++;
+//                }
+//                if (count != 0){
+//                    float average = sum/count;
+//                    ratingBar.setRating(average);
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
+
+//    private void showRatingDialog() {
+//        new AppRatingDialog.Builder()
+//                .setPositiveButtonText("Submit")
+//                .setNegativeButtonText("Cancel")
+//                .setNoteDescriptions(Arrays.asList("Very Bad", "Not good", "Quit OK", "Very good", "Excellent"))
+//                .setDefaultRating(1)
+//                .setTitle("Rate this food")
+//                .setDescription("Please select some stars and give your feedback")
+//                .setTitleTextColor(R.color.colorPrimary)
+//                .setDescriptionTextColor(R.color.colorPrimary)
+//                .setHint("Please write your comments here")
+//                .setHintTextColor(R.color.colorAccent)
+//                .setCommentTextColor(android.R.color.white)
+//                .setCommentBackgroundColor(R.color.colorPrimary)
+//                .setWindowAnimation(R.style.RatingDialogFadeAnim)
+//                .create(FoodDetail.this)
+//                .show();
+//
+//    }
+
+    private void getDetailFood(String foodId) {
+        foods.child(foodId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                currentFood = dataSnapshot.getValue(Food.class);
+                Picasso.with(getBaseContext()).load(currentFood.getImage())
+                        .into(food_image);
+
+                collapsingToolbarLayout.setTitle(currentFood.getName());
+                food_price.setText(currentFood.getPrice());
+
+                food_name.setText(currentFood.getName());
+                food_description.setText(currentFood.getDescription());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+//
+//    @Override
+//    public void onPositiveButtonClicked(int value, String comments) {
+//        final Rating rating = new Rating(Common.currentUser.getPhone(),
+//                FoodId,
+//                String.valueOf(value),
+//                comments
+//        );
+//
+//        ratingTbl.push()
+//                .setValue(rating)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        Toast.makeText(FoodDetail.this, "Thank you for submit", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//        /*
+//        ratingTbl.child(Common.currentUser.getPhone()).addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                if(dataSnapshot.child(Common.currentUser.getPhone()).exists()){
+//                    ratingTbl.child(Common.currentUser.getPhone()).removeValue();
+//                    ratingTbl.child(Common.currentUser.getPhone()).setValue(rating);
+//                }
+//                else {
+//                    ratingTbl.child(Common.currentUser.getPhone()).setValue(rating);
+//                }
+//                Toast.makeText(FoodDetail.this, "Thank you for submit", Toast.LENGTH_SHORT).show();
+//            }
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//            }
+//        });*/
+//    }
+//
+//    @Override
+//    public void onNegativeButtonClicked() {
+//
+//    }
 }
